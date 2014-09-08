e4bc4aee8240500f2c89d2c62b6a1d7ace0b42d6
Chris Holden <ceholden@gmail.com>
2014-09-08 00:56:13 -0400

    Basic implementation of algorithm image validation
    - ABC now has basic image validation
    - Added description field to ABC for compositors

--- image_compositor/src/compositors/composite_algorithm.py
+++ image_compositor/src/compositors/composite_algorithm.py
@@ -9,7 +9,7 @@ from osgeo import gdal
 gdal.AllRegister()
 gdal.UseExceptions()
 
-logger = logging.getLogger('image_compositor')
+logger = logging.getLogger(__name__)
 
 
 class Compositor(object):
@@ -37,14 +37,9 @@ class Compositor(object):
     def __repr__(self):
         return "A compositing algorithm"
 
-    def __init__(self, files):
-        """ Initialize compositing algorithm with list of files
-
-        Args:
-          files (list): list of filenames to be used in composite
-
-        """
-        self.files = files
+    @abc.abstractproperty
+    def description(self):
+        return
 
     @abc.abstractproperty
     def input_info(self):
@@ -54,7 +49,34 @@ class Compositor(object):
     def input_info_str(self):
         return
 
-    def validate_images(self):
+    def _get_image_attributes(self, image):
+        """ Return image attributes useful for validation
+
+        Args:
+          image (str): filename of image
+
+        Returns:
+          attributes (tuple): tuple of projection, pixel sizes, UL x and y
+            posting, and the number of bands
+
+        """
+        ds = gdal.Open(image, gdal.GA_ReadOnly)
+        proj = ds.GetProjection()
+
+        gt = ds.GetGeoTransform()
+        # Only deal with north-up images for right now
+        if gt[2] != 0 or gt[4] != 0:
+            logger.warning('Only supporting north-up images for now')
+            raise ValueError('Only can use north-up images')
+
+        px_size, py_size = gt[1], gt[5]
+        ul_x, ul_y = gt[0], gt[3]
+
+        nband = ds.RasterCount
+
+        return (proj, px_size, py_size, ul_x, ul_y, nband)
+
+    def validate_images(self, images):
         """ Validates which images in self.files can be used for composites
 
         The default compositing algorithm implementation will not perform
@@ -66,13 +88,59 @@ class Compositor(object):
             - common pixel postings (e.g., offset by whole number of pixels)
             - common number of bands
 
+        Args:
+          images (list): list of filenames for images to be validated
+
         Returns:
           valid (list): True or False for each file in self.file if file is
             usable within the algorithm
 
         """
-        # TODO
-        return
+        valid = []
+        # Get attributes from first image
+        for image in images:
+            try:
+                self.proj, \
+                    self.px_size, self.py_size, \
+                    self.ul_x, self.ul_y, \
+                    self.nband = \
+                    self._get_image_attributes(images)
+            except ValueError:
+                valid.append(False)
+            else:
+                valid.append(True)
+                break
+
+        for image in images[len(valid):]:
+            _valid = True
+
+            _proj, _px, _py, _ul_x, _ul_y, _nband = \
+                self._get_image_attributes(image)
+
+            if _proj != self.proj:
+                logger.warning('Image {i} has different projection than base \
+                    image'.format(i=image))
+                _valid = False
+
+            if _px != self.px_size or _py != self.py_size:
+                logger.warning('Image {i} has different pixel size than base \
+                    image'.format(i=image))
+                _valid = False
+
+            if (self.ul_x - _ul_x) % self.px_size != 0 or \
+                    (self.ul_y - _ul_y) % self.py_size != 0:
+                logger.warning('Image {i} has different posting than base\
+                    image'.format(i=image))
+                _valid = False
+
+            if _nband != self.nband:
+                logger.warning('Image {i} has different number of bands than \
+                    base image'.format(i=image))
+                _valid = False
+
+            valid.append(_valid)
+
+        return valid
 
     @abc.abstractmethod
     def process_image(self, ncpu=1):
